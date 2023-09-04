c8b712b5f0cbb3a6143d1c60abd71130f2f44986
aaryakini <kinia313@newschool.edu>
2023-09-03 22:37:20 -0400

    bunch of edits

--- collection/index.js
+++ collection/index.js
@@ -5,95 +5,77 @@ var Airtable = require('airtable');
 var base = new Airtable({apiKey: 'pat56tfP2XEHesKmi.26e076d65e5d850669b7535104b874ded3100bbcfac74a895caf6999eb8b4c15'}).base('appjAt0R0NzejkdO3');
 
 base('collection').select({
+    // maxRecords: 3,
     view: "Grid view"
-}).eachPage(function page(records, fetchNextPage) {
+}).eachPage(async function page(records, fetchNextPage) {
     // This function (`page`) will get called for each page of records.
 
     records.forEach(function(record) {
-        console.log(record.get('name'));
+        console.log(record.get('photo')[0].url);
 
         let templateCopy = template.cloneNode(true);
         container.appendChild(templateCopy);
 
         templateCopy.querySelector(".title").append(record.get('name'));
-		templateCopy.querySelector(".image").append(record.get('photo'));
-        if(record.get('source') != ""){
-            templateCopy.querySelector("source").append(record.get('source'));
-        }else{
-            templateCopy.querySelector("source").append("");
+		templateCopy.querySelector('img').src = record.get('photo')[0].url;
+        if (record.get('category') == "desi"){
+            templateCopy.setAttribute("style", "-webkit-filter: drop-shadow(5px 5px 5px yellow);");
+
+        }else if (record.get('category') == "trash"){
+            templateCopy.setAttribute("style", "-webkit-filter: drop-shadow(5px 5px 5px pink);");
+
+        }else if (record.get('category') == "kathak"){
+            templateCopy.setAttribute("style", "-webkit-filter: drop-shadow(5px 5px 5px orange);");
+
+        }else if (record.get('category') == "singapore"){
+            templateCopy.setAttribute("style", "-webkit-filter: drop-shadow(5px 5px 5px red);");
+
+        }else if (record.get('category') == "labor"){
+            templateCopy.setAttribute("style", "-webkit-filter: drop-shadow(5px 5px 5px blue);");
+
+        }else if (record.get('category') == "misc"){
+            templateCopy.setAttribute("style", "-webkit-filter: drop-shadow(5px 5px 5px green);");
+
+        }else if (record.get('category') == "family"){
+            templateCopy.setAttribute("style", "-webkit-filter: drop-shadow(5px 5px 5px violet);");
+
+        }else if (record.get('category') == "friends"){
+            templateCopy.setAttribute("style", "-webkit-filter: drop-shadow(5px 5px 5px magenta);");
+
         }
+        if(record.get('source') != ""){
+            templateCopy.querySelector(".source").append(record.get('source'));
+        }else{
+            let element = templateCopy.getElementsByClassName(className)[0];
+            templateCopy.removeChild(element);
+        }
+
+        // templateCopy.querySelector(".source").append(record.get('source'));
 		templateCopy.querySelector(".desc").append(record.get('desc'));
         templateCopy.removeAttribute('id');
 
-        getColour(record.get('category'));
+        templateCopy.style.left = getPosX();
+        templateCopy.style.top = getPosY();
 
-        // console.log('Retrieved', record.get('name'), ' ' , record.get('relationship'), ' ' , record.get('testimonial'));
+        console.log('Retrieved', record.get('name'));
     });
 
     // To fetch the next page of records, call `fetchNextPage`.
     // If there are more records, `page` will get called again.
     // If there are no more records, `done` will get called.
     fetchNextPage();
-
-
-    function getColour(category){
-        let cat = category;
-        if (category == "desi"){
-            templatecopy.setAttribute("style", "-webkit-filter: drop-shadow(5px 5px 5px yellow);");
-
-        }else if (category == "trash"){
-            templatecopy.setAttribute("style", "-webkit-filter: drop-shadow(5px 5px 5px pink);");
-
-        }else if (category == "kathak"){
-            templatecopy.setAttribute("style", "-webkit-filter: drop-shadow(5px 5px 5px orange);");
-
-        }else if (category == "singapore"){
-            templatecopy.setAttribute("style", "-webkit-filter: drop-shadow(5px 5px 5px red);");
-
-        }else if (category == "labor"){
-            templatecopy.setAttribute("style", "-webkit-filter: drop-shadow(5px 5px 5px blue);");
-
-        }else if (category == "misc"){
-            templatecopy.setAttribute("style", "-webkit-filter: drop-shadow(5px 5px 5px green);");
-
-        }else if (category == "family"){
-            templatecopy.setAttribute("style", "-webkit-filter: drop-shadow(5px 5px 5px violet);");
-
-        }else if (category == "friends"){
-            templatecopy.setAttribute("style", "-webkit-filter: drop-shadow(5px 5px 5px magenta);");
-
-        }
-    }
 }, function done(err) {
     if (err) { console.error(err); return; }
 });
 
-let hide = document.getElementsByClassName("tooltip")[0];
-hide.classList.add("hide");
+function getPosX(){
+   let posX = Math.floor(Math.random() * ((container.clientWidth - 30) - 30) + 30);
+   return posX + "px";
+}
 
-let cards = [];
-let posX;
-let posY;
-let windowWidth = window.innerWidth;
-let windowHeight = window.innerHeight;
-
-let w = windowWidth - (0.25*windowWidth);
-let h = windowHeight - (0.25*windowHeight);
-
-console.log(w);
-console.log(h);
-
-cards = document.getElementsByClassName("tooltip");
-
-function origPositions(){
-    for (let i = 0; i < cards.length; i++){
-        posX = Math.floor(Math.random()*w);
-        posY = Math.floor(Math.random()*h);
-        cards[i].style.top = posY + "px";
-        cards[i].style.left = posX + "px";
-        
-        console.log("Number " + i + " | posX = " + posX + "px & posY = " + posY + "px");
-    }
+function getPosY(){
+    let posY = Math.floor(Math.random() * ((container.clientHeight - 30) - 30) + 30);
+    return posY + "px";
 }
 
 /***************************************************************************************
@@ -104,7 +86,7 @@ function origPositions(){
 ***************************************************************************************/
 
 var draggableElements = document.getElementsByClassName("tooltip");
-console.log("draggies = " + draggableElements.length); //checking that array exists and is populated
+console.log("draggable elements = " + draggableElements.length); //checking that array exists and is populated
 
 for(var i = 0; i < draggableElements.length; i++){
     dragElement(draggableElements[i]);
